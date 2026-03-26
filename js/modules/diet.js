61c41f6b64446043f3f93e26b2889624baaa5141
Myhealthtracker <nswer0719@gmail.com>
2026-03-26 22:36:14 +0800

    Update diet.js

--- js/modules/diet.js
+++ js/modules/diet.js
@@ -1,11 +1,21 @@
+// js/modules/diet.js
 export const DietModule = {
-    calculateTDEE(data) {
-        const { age, gender, height, weight, activity } = data;
+    /**
+     * 使用 Mifflin-St Jeor 公式計算 BMR 與 TDEE
+     */
+    calculate(stats) {
+        const { age, gender, height, weight, activity } = stats;
         
-        // Mifflin-St Jeor 公式
+        // 基礎代謝率 BMR
         let bmr = (10 * weight) + (6.25 * height) - (5 * age);
         bmr = (gender === 'male') ? bmr + 5 : bmr - 161;
         
-        return Math.round(bmr * activity);
+        // 總消耗 TDEE
+        const tdee = bmr * activity;
+        
+        return {
+            bmr: Math.round(bmr),
+            tdee: Math.round(tdee)
+        };
     }
 };
