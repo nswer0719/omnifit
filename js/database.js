08cae7a3568e062c2ca06c24685b1f8ab3e0b725
Myhealthtracker <nswer0719@gmail.com>
2026-03-26 22:49:39 +0800

    Add files via upload

---
+++ js/database.js
@@ -0,0 +1,11 @@
+// js/database.js
+export const FoodDatabase = [
+    { id: 'f001', name: '雞胸肉', kcal: 165, unit: '100g', protein: 31, fat: 3.6, carbs: 0 },
+    { id: 'f002', name: '白飯', kcal: 130, unit: '100g', protein: 2.7, fat: 0.3, carbs: 28 },
+    { id: 'f003', name: '雞蛋', kcal: 78, unit: '1個', protein: 6.3, fat: 5.3, carbs: 0.6 },
+    { id: 'f004', name: '香蕉', kcal: 89, unit: '100g', protein: 1.1, fat: 0.3, carbs: 23 },
+    { id: 'f005', name: '地瓜', kcal: 86, unit: '100g', protein: 1.6, fat: 0.1, carbs: 20 },
+    { id: 'f006', name: '全脂牛奶', kcal: 61, unit: '100g', protein: 3.2, fat: 3.3, carbs: 4.8 },
+    { id: 'f007', name: '酪梨', kcal: 160, unit: '100g', protein: 2, fat: 15, carbs: 9 },
+    { id: 'f008', name: '燕麥', kcal: 389, unit: '100g', protein: 16.9, fat: 6.9, carbs: 66 }
+];
