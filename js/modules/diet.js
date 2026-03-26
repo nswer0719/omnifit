a23a0aef874e9fd0b3835d314d501be0df64a33a
Myhealthtracker <nswer0719@gmail.com>
2026-03-26 22:42:34 +0800

    Update diet.js

--- js/modules/diet.js
+++ js/modules/diet.js
@@ -1,21 +1,30 @@
 // js/modules/diet.js
 export const DietModule = {
     /**
-     * 使用 Mifflin-St Jeor 公式計算 BMR 與 TDEE
+     * 使用 Mifflin-St Jeor 公式計算
+     * 男: BMR = 10w + 6.25h - 5a + 5
+     * 女: BMR = 10w + 6.25h - 5a - 161
      */
     calculate(stats) {
         const { age, gender, height, weight, activity } = stats;
         
-        // 基礎代謝率 BMR
         let bmr = (10 * weight) + (6.25 * height) - (5 * age);
         bmr = (gender === 'male') ? bmr + 5 : bmr - 161;
         
-        // 總消耗 TDEE
         const tdee = bmr * activity;
         
         return {
             bmr: Math.round(bmr),
             tdee: Math.round(tdee)
+        };
+    },
+
+    // 未來可以在這裡新增：建議蛋白質攝取、水分計算等
+    getMacroAdvice(tdee) {
+        return {
+            protein: Math.round(tdee * 0.3 / 4), // 30% 蛋白質
+            carbs: Math.round(tdee * 0.4 / 4),   // 40% 碳水
+            fat: Math.round(tdee * 0.3 / 9)      // 30% 脂肪
         };
     }
 };
