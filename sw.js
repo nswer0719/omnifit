ad38fd1175aaef4660f23addd8e4794a3b24a47c
Myhealthtracker <nswer0719@gmail.com>
2026-03-26 22:15:22 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,26 +1,20 @@
-const CACHE_NAME = 'omnifit-v1.0.0';
-const ASSETS_TO_CACHE = [
-    './index.html',
-    './style.css',
-    './database.js',
-    './app.js',
-    './manifest.json',
-    'https://cdn.tailwindcss.com',
-    'https://cdn.jsdelivr.net/npm/chart.js'
+const CACHE_NAME = 'omnifit-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './css/style.css',
+  './js/app.js',
+  './manifest.json'
 ];
 
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.addAll(ASSETS_TO_CACHE);
-        })
-    );
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request).then((response) => {
-            return response || fetch(event.request);
-        })
-    );
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
