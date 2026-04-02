30789047983a64c61d0c09c653b7ace5a9cb37ad
Myhealthtracker <nswer0719@gmail.com>
2026-04-02 21:52:08 +0800

    Add files via upload

---
+++ js/training_db.js
@@ -0,0 +1,17 @@
+// js/training_db.js
+const trainingDatabase = [
+    // 胸部 (Chest)
+    "槓鈴臥推 (Barbell Bench Press)", "上斜槓鈴臥推 (Incline Barbell Bench Press)", "下斜槓鈴臥推 (Decline Bench Press)", "啞鈴臥推 (Dumbbell Bench Press)", "上斜啞鈴臥推 (Incline Dumbbell Press)", "啞鈴飛鳥 (Dumbbell Flyes)", "機械胸推 (Machine Chest Press)", "蝴蝶機飛鳥 (Pec Deck Fly)", "滑輪飛鳥 (Cable Crossover)", "雙槓臂屈伸 (Dips)",
+    // 背部 (Back)
+    "引體向上 (Pull-ups)", "反手引體向上 (Chin-ups)", "滑輪下拉 (Lat Pulldown)", "直臂下拉 (Straight Arm Pulldown)", "槓鈴划船 (Barbell Row)", "啞鈴單臂划船 (Single Arm Dumbbell Row)", "坐姿划船 (Seated Cable Row)", "T字槓划船 (T-Bar Row)", "滑輪面拉 (Face Pull)", "羅馬椅挺身 (Back Extension)",
+    // 腿部 (Legs)
+    "槓鈴深蹲 (Barbell Squat)", "頸前深蹲 (Front Squat)", "高腳杯深蹲 (Goblet Squat)", "保加利亞分腿蹲 (Bulgarian Split Squat)", "啞鈴弓箭步 (Dumbbell Lunges)", "腿推機 (Leg Press)", "羅馬尼亞硬舉 (Romanian Deadlift, RDL)", "傳統硬舉 (Conventional Deadlift)", "相撲硬舉 (Sumo Deadlift)", "坐姿腿伸屈 (Leg Extension)", "俯臥腿彎舉 (Leg Curl)", "站姿小腿提踵 (Standing Calf Raise)", "坐姿小腿提踵 (Seated Calf Raise)", "臀推 (Hip Thrust)", "機械外展 (Hip Abductor)",
+    // 肩部 (Shoulders)
+    "槓鈴肩推 (Overhead Press)", "坐姿啞鈴肩推 (Seated Dumbbell Press)", "阿諾推舉 (Arnold Press)", "啞鈴側平舉 (Lateral Raise)", "滑輪側平舉 (Cable Lateral Raise)", "啞鈴前平舉 (Front Raise)", "俯身側平舉 (Reverse Pec Deck/Rear Delt Fly)", "槓鈴聳肩 (Barbell Shrug)", "啞鈴聳肩 (Dumbbell Shrug)", "地雷管肩推 (Landmine Press)",
+    // 手臂 (Arms)
+    "槓鈴二頭彎舉 (Barbell Bicep Curl)", "啞鈴交替彎舉 (Alternate Dumbbell Curl)", "錘式彎舉 (Hammer Curl)", "牧師椅彎舉 (Preacher Curl)", "滑輪二頭彎舉 (Cable Curl)", "窄距臥推 (Close-Grip Bench Press)", "法式彎舉 (Skull Crusher)", "啞鈴頸後臂屈伸 (Overhead Triceps Extension)", "滑輪三頭下壓 (Triceps Pushdown)", "繩索三頭下壓 (Rope Pushdown)",
+    // 核心與其他 (Core & Misc)
+    "仰臥起坐 (Sit-ups)", "捲腹 (Crunches)", "反向捲腹 (Reverse Crunch)", "俄羅斯轉體 (Russian Twist)", "平板支撐 (Plank)", "側平板支撐 (Side Plank)", "懸垂舉腿 (Hanging Leg Raise)", "健腹輪 (Ab Roller)", "壺鈴擺盪 (Kettlebell Swing)", "土耳其起立 (Turkish Get-Up)",
+    // 有氧與器材 (Cardio)
+    "跑步機 (Treadmill)", "橢圓機 (Elliptical)", "飛輪車 (Spin Bike)", "划船機 (Rowing Machine)", "登階機 (Stairmaster)"
+];
